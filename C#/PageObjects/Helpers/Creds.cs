bbea16e41d30cc05572a7ef400e5425d6e65e5f6
kreisfahrer <kreisfahrer@gmail.com>
2016-05-12 01:03:40 +0300

    Added c# examples

---
+++ C#/PageObjects/Helpers/Creds.cs
@@ -0,0 +1,15 @@
+﻿namespace PageObjects.Helpers
+{
+    public static class Creds
+    {
+        public static string User 
+        {
+            get { return "üser"; }
+        }
+
+        public static string Pass
+        {
+            get { return "password"; } // add your own creds provider
+        }
+    }
+}
---
+++ C#/PageObjects/Test/LoginTest.cs
@@ -0,0 +1,60 @@
+﻿using Bumblebee.Extensions;
+using Bumblebee.Setup;
+using Bumblebee.Setup.DriverEnvironments;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using PageObjects.Helpers;
+using PageObjects.Pages;
+
+namespace PageObjects.Test
+{
+    public class LoginTest
+    {
+        private const string BaseUrl = "https://www.pandadoc.com/";
+        private readonly string User = Creds.User;
+        private readonly string Pass = Creds.Pass;
+
+        [Test]
+        public void SimpleLoginFactoryTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(BaseUrl);
+            var loginPage = new LoginFactoryPage(driver);
+            loginPage.Login(User, Pass);
+            Assertions.AssertSignedUp(driver);
+        }
+
+        [Test]
+        public void SimpleLoginTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(BaseUrl);
+            var loginPage = new LoginPage(driver);
+            loginPage.Login(User, Pass);
+            Assertions.AssertSignedUp(driver);
+        }
+
+        [Test]
+        public void HtmlLoginTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(BaseUrl);
+            var loginPage = new HtmlLoginPage(driver);
+            loginPage.Login(User, Pass);
+            Assertions.AssertSignedUp(driver);
+        }
+
+        [Test]
+        public void given_valid_credentials_when_logging_in_then_logged_in()
+        {
+            Threaded<Session>
+              .With<Firefox>()
+              .NavigateTo<BumblebeeLoginPage>(BaseUrl)
+              .UserInput.EnterText(User)
+              .PasswordInput.EnterText(Pass)
+              .LoginButton.Click<LoggedInPage>()
+              .VerifyPresenceOf("User avatar", By.CssSelector(".header-avatar"));
+        }
+    }
+}
